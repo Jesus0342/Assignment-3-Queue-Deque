9381a47f791f596d550c054c13216b48d62ded01
Jesus0342 <jsanchez97@saddleback.edu>
2017-09-19 12:41:18 -0700

    finished and tested all requirements

--- CircularVector.h
+++ CircularVector.h
@@ -9,8 +9,13 @@ template <class Type>
 class CircularVector
 {
 public:
+	// Default constructor.
 	CircularVector(int vectorSize = 100);
+
+	// Destructor.
 	~CircularVector();
+
+	//
 	bool empty();
 	bool full();
 	int size();
@@ -22,6 +27,7 @@ public:
 	void erase(int position);
 	Type front();
 	Type back();
+	Type at(int position);
 
 private:
 	int maxSize;
@@ -36,6 +42,8 @@ private:
 template <class Type>
 CircularVector<Type>::CircularVector(int vectorSize)
 {
+	// Creates a vector of size 100 by default if no size is specified or if
+	// the size is negative, else creates a vector of the specified size.
 	if(vectorSize <= 0)
 	{
 		cout << "Size of the vector cannot be negative.\n"
@@ -48,12 +56,19 @@ CircularVector<Type>::CircularVector(int vectorSize)
 		maxSize = vectorSize;
 	}
 
+	// Assigns maxSize to head so all insertions at the front of the vector
+	// happen from maxSize - 1 to 0.
 	head = maxSize;
 
+	// Assigns rear to be 1 below maxSize so that all insertions at the back of
+	// the vector happen from 0 to maxSize - 1.
 	rear  = maxSize - 1;
 
+	// Initializes the size of the vector to 0.
 	count = 0;
 
+	// Creates a pointer to a dynamic array of the size specified at object
+	// instantiation.
 	list = new Type[maxSize];
 }
 
@@ -78,32 +93,43 @@ bool CircularVector<Type>::full()
 template <class Type>
 void CircularVector<Type>::resize()
 {
+	// Creates a dynamic array that is twice the size of the initial array.
 	Type *copyList = new Type [maxSize * 2];
 	Type *temp = list;
 
+	// Makes head = 0 if no push_front() insertions were done.
 	if(head >= maxSize)
 	{
 		head = 0;
 	}
 
+	// Copies the contents on the old list into the new list.
 	for(int i = 0; i < count; i++)
 	{
 		copyList[i] = list[head];
 
 		head++;
 
+		// Resets head to 0 in order to loop it back to the front of the dynamic
+		// array, but to continue on in the circular array.
 		if(head >= maxSize)
 		{
 			head = 0;
 		}
 	}
 
+	// Points list to the new list that is twice as big.
 	list = copyList;
 
+	// Doubles max size.
 	maxSize *= 2;
 
+	// Assings head back to maxSize just as in the constructor.
 	head = maxSize;
 
+	// Assigns rear to be 1 less than half the new size of the vector since
+	// this is always the index where the last value of the old list will be
+	// in the new list.
 	rear = (maxSize / 2) - 1;
 
 	delete temp;
@@ -118,37 +144,47 @@ int CircularVector<Type>::size()
 template <class Type>
 void CircularVector<Type>::push_back(const Type item)
 {
+	// Doubles the size of the vector if it is full.
 	if(full())
 	{
 		resize();
 	}
 
+	// Changes the value of rear to be the next index up in the dynamic array
+	// starting from 0 and going to maxSize - 1.
 	rear = (rear + 1) % maxSize;
 
+	// Increments count to signify that the vector has grown by 1.
 	count++;
 
+	// Assigns the new value into the appropriate index of the circular array.
 	list[rear] = item;
 }
 
 template <class Type>
 void CircularVector<Type>::push_front(const Type item)
 {
+	// Doubles the size of the vector if it is full.
 	if(full())
 	{
 		resize();
 	}
 
+	// Changes the value of head to be the next index down in the dynamic array
+	// starting from maxSize - 1 and going to 0.
 	head = (head - 1) % maxSize;
 
+	// Increments count to signify that the vector has grown by 1.
 	count++;
 
+	// Assigns the new value into the appropriate index of the circular array.
 	list[head] = item;
 }
 
-// FIX THIS...
 template <class Type>
 void CircularVector<Type>::insert(int position, const Type item)
 {
+	// Escapes the function if an inalid position is entered.
 	if(position >= maxSize || position < 0)
 	{
 		cout << "\nInvalid position.\n\n";
@@ -156,6 +192,7 @@ void CircularVector<Type>::insert(int position, const Type item)
 		return;
 	}
 
+	// Doubles the size of the vector if it is full.
 	if(full())
 	{
 		resize();
@@ -165,8 +202,6 @@ void CircularVector<Type>::insert(int position, const Type item)
 	// at either end of the vector, else inserts the item at specified position.
 	if(position == 0)
 	{
-		cout << "0 was entered calling pushhead()\n";
-
 		push_front(item);
 	}
 	else if(position == maxSize - 1)
@@ -175,41 +210,59 @@ void CircularVector<Type>::insert(int position, const Type item)
 	}
 	else
 	{
+		// Makes head = 0 if no push_front() insertions were done.
 		if(head == maxSize)
 		{
 			head = 0;
 		}
 
+		// Adds the value of head to position so that the correct index in the
+		// circular array can be accessed.
 		position += head;
 
+		// Subtracts maxSize from position if position is out of bounds so that
+		// position is reset at the beginning of the array but moves to the
+		// correct index of the circular array.
 		if(position >= maxSize)
 		{
 			position -= maxSize;
 		}
 
-		int newRear = rear + 1;
+		// Assigns the value of rear to the index that will access each element
+		// in the array.
+		int moveIndex = rear;
 
-		while(rear >= position)
+		// Increments rear to the next index since a new value is being inserted.
+		rear++;
+
+		// Moves all values after the position where the new value will be
+		// inserted to the right by 1.
+		while(moveIndex >= position)
 		{
-			if(rear < 0)
+			// Brings move index to maxSize - 1 once it is less than 0 to wrap
+			// around to the back of the dynamic array but continue on back
+			// in the circular array.
+			if(moveIndex < 0)
 			{
-				rear = maxSize - 1;
+				moveIndex = maxSize - 1;
 
-				list[0] = list[rear];
+				list[0] = list[moveIndex];
 			}
 			else
 			{
-				list[rear + 1] = list[rear];
+				// Assigns the value of the current index to the next cell in
+				// the array.
+				list[moveIndex + 1] = list[moveIndex];
 			}
 
-			rear--;
+			moveIndex--;
 		}
 
+		// Assigns the value to the correct position in the circular array once
+		// all values have been shifted.
 		list[position] = item;
 
-		rear = newRear;
-
-		// count++ goes inside else because push_front() has its own count++
+		// Increments count to signify that the vector has grown by 1.
 		count++;
 	}
 }
@@ -234,6 +287,7 @@ void CircularVector<Type>::pop_back()
 {
 	count--;
 
+	//
 	if(rear == 0)
 	{
 		rear = maxSize - 1;
@@ -290,7 +344,7 @@ void CircularVector<Type>::erase(int position)
 		// correct index of the circular array.
 		if(position >= maxSize)
 		{
-			position -=maxSize;
+			position -= maxSize;
 		}
 
 		// Moves all values back by one while position has not reached the end
@@ -339,6 +393,31 @@ template <class Type>
 Type CircularVector<Type>::back()
 {
 	return list[rear];
+}
+
+template <class Type>
+Type CircularVector<Type>::at(int position)
+{
+	// Performs a boundary check and returns -1 if the indicated index is out
+	// of bounds.
+	if(position < 0 || position >= maxSize)
+	{
+		cout << "\nOut of bounds!\n\n";
+
+		return -1;
+	}
+
+	position += head;
+
+	// Subtracts maxSize from position if position is out of bounds so that
+	// position is reset at the beginning of the array but moves to the
+	// correct index of the circular array.
+	if(position >= maxSize)
+	{
+		position -= maxSize;
+	}
+
+	return list[position];
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -23,10 +23,21 @@
  *
  * OUTPUT:
  *    stringQueue after each insertion and deletion.
- *
+ *	  intVector after each insertion and deletion.
  *****************************************************************************/
 int main()
 {
+	cout << "NAME          : Jesus Sanchez\n"
+		 << "ASSIGNMENT #3 : Queue DeQue\n"
+		 << "CLASS         : CS1D - MW: 3:00pm\n"
+		 << "DUE DATE      : 09/20/2017\n\n"
+		 << "DESCRIPTION : This program will implement the queue container\n"
+		    "class based on a linked deque class whose functions will be\n"
+		    "wrapped in the Queue class and test the functions. Then, the\n"
+		    "vector ADT will be implemented using an expandable circular array.\n"
+		    "The vector will be printed after each insertion and deletion and\n"
+		    "all other necessary functions will also be implemented and tested.\n\n";
+
 	LinkedQueueType<string> stringQueue;
 
 	string string1 = "A man, a plan, a canal, Panama";
@@ -78,21 +89,106 @@ int main()
 
 	cout << "***** Part 2 *****\n\n";
 
-	CircularVector<int> test(3);
+	// Initializes a vector of size 2.
+	CircularVector<int> intVector(2);
 
-	for(int i = 0; i < 7; i++)
+	// Prints a statement if the vector is empty to test the empty() method.
+	if(intVector.empty())
 	{
-		test.push_front(i + 1);
+		cout << "The vector is empty!\n\n";
 	}
 
-	cout << "size " <<  test.size() << endl;
-
-	while(!test.empty())
+	// Testing push_front() method.
+	cout << "Adding values to the front of the vector...\n";
+	for(int i = 5; i >= 1; i--)
 	{
-		cout << test.front() << endl;
-
-		test.pop_front();
+		intVector.push_front(i);
 	}
+
+	// Prints the values in the vector to test the size() and at() methods.
+	for(int i = 0; i < intVector.size(); i++)
+	{
+		cout << intVector.at(i) << " ";
+	}
+
+	// Tests the pop_front() method and prints the values in the vector after
+	// using the function twice.
+	cout << "\n\nPopping the first 2 values off the vector...\n";
+	intVector.pop_front();
+	intVector.pop_front();
+
+	for(int i = 0; i < intVector.size(); i++)
+	{
+		cout << intVector.at(i) << " ";
+	}
+
+	// Test the push_back() method and adds values to the back of the vector.
+	cout << "\n\nAdding values to the back of the vector...\n";
+	for(int i = 6; i <= 9; i++)
+	{
+		intVector.push_back(i);
+	}
+
+	for(int i = 0; i < intVector.size(); i++)
+	{
+		cout << intVector.at(i) << " ";
+	}
+
+	// Tests the pop_back() function and prints the values in the vector after
+	// removing the last 3 values.
+	cout << "\n\nRemoving the last 3 values from the vector...\n";
+	for(int i = 1; i <= 3; i++)
+	{
+		intVector.pop_back();
+	}
+
+	for(int i = 0; i < intVector.size(); i++)
+	{
+		cout << intVector.at(i) << " ";
+	}
+
+	// Tests the insert() method by inserting a number at the specified position
+	// and prints the vector after the insertion.
+	cout << "\n\nInserting a new item at position 2...\n";
+	intVector.insert(2, 10);
+
+	for(int i = 0; i < intVector.size(); i++)
+	{
+		cout << intVector.at(i) << " ";
+	}
+
+	// Tests the erase() method by deleting the value in the indicated position
+	// and printing the values in the vector.
+	cout << "\n\nErasing the value at position 1...\n";
+	intVector.erase(1);
+
+	for(int i = 0; i < intVector.size(); i++)
+	{
+		cout << intVector.at(i) << " ";
+	}
+
+	// Erases another value in the vector and prints the vector.
+	cout << "\n\nErasing the value at position 2...\n";
+	intVector.erase(2);
+
+	for(int i = 0; i < intVector.size(); i++)
+	{
+		cout << intVector.at(i) << " ";
+	}
+
+	// Direct test of the size() method.
+	cout << "\n\nThere are " << intVector.size() << " values in the vector.\n\n";
+
+	cout << "Popping all values off the vector.\n\n";
+
+	// Pops all of the values off the vector.
+	while(!intVector.empty())
+	{
+		intVector.pop_back();
+	}
+
+	// Prints the number of values in the vector.
+	cout << "There are " << intVector.size() << " values in the vector.\n\n";
 
 	return 0;
 }
